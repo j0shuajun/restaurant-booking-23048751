e405a8f0745b0ef1c5604205cf6c4c6385fcf38f
j0shuajun <kimmj1e34@naver.com>
2026-07-02 13:58:18 +0900

    replace custome SMS/Mail mocker using pytest

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -43,17 +43,17 @@ def booking_scheduler():
 
 
 @pytest.fixture
-def booking_scheduler_with_sms_mock():
+def booking_scheduler_with_sms_mock(mocker):
     booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
-    testable_sms_sender = TestableSmsSender()
+    testable_sms_sender = mocker.Mock()
     booking_scheduler.set_sms_sender(testable_sms_sender)
     return booking_scheduler, testable_sms_sender
 
 
 @pytest.fixture
-def booking_scheduler_with_mail_mock():
+def booking_scheduler_with_mail_mock(mocker):
     booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
-    testable_mail_sender = TestableMailSender()
+    testable_mail_sender = mocker.Mock()
     booking_scheduler.set_mail_sender(testable_mail_sender)
     return booking_scheduler, testable_mail_sender
 
@@ -100,7 +100,7 @@ def test_예약완료시_SMS는_무조건_발송(booking_scheduler_with_sms_mock
 
     booking_scheduler.add_schedule(schedule)
 
-    assert sms_mock.send_called
+    sms_mock.send.assert_called()
 
 
 def test_이메일이_없는_경우에는_이메일_미발송(booking_scheduler_with_mail_mock, customer):
@@ -109,7 +109,7 @@ def test_이메일이_없는_경우에는_이메일_미발송(booking_scheduler_
 
     booking_scheduler.add_schedule(schedule)
 
-    assert mail_mock.send_mail_count == 0
+    mail_mock.send_mail.assert_not_called()
 
 
 def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_with_mail_mock, customer_with_mail):
@@ -118,7 +118,7 @@ def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_wit
 
     booking_scheduler.add_schedule(schedule)
 
-    assert mail_mock.send_mail_count == 1
+    mail_mock.send_mail.assert_called()
 
 
 def test_현재날짜가_일요일인_경우_예약불가_예외처리(customer):
